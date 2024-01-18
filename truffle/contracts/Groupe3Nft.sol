3e6788758b05dc1230ce96d296971660fadfa294
Thomas Aubert <thomas.aubert@edu.devinci.fr>
2024-01-18 16:58:10 +0100

    :bug: Update contract

--- truffle/contracts/Groupe3Nft.sol
+++ truffle/contracts/Groupe3Nft.sol
@@ -12,31 +12,40 @@ contract GROUPE3 is ERC721, ERC721URIStorage, Ownable {
     string[] private _imageURIs;
     mapping(uint256 => bool) private _imageAssigned;
 
-    constructor(address initialOwner)
-        ERC721("GROUPE3", "GP3")
-        Ownable(initialOwner)
-    {
+    constructor(
+        address initialOwner
+    ) ERC721("GROUPE3", "GP3") Ownable(initialOwner) {
         _imageURIs = [
-            'ipfs://bafybeicefp4gw74adzrneu4mgm4crhdorablnpndrp5jz735gy3gfypkba',
-            'ipfs://bafybeigu7j3q3bur7xbxd4jrebkgin63zdj2jvnyu3xvp5enozzgznprqm',
-            'ipfs://bafybeiddqbyqcwzcw3o7otbmdncfkix2wtbgsts7ol3uggeztblq2na6ga',
-            'ipfs://bafybeigr65ngxeeuak3dyfu6rghvzly7pyxeqwqdl4u4mxjb5xjua4ksia',
-            'ipfs://bafybeicrtxwekyquafup53vi5pgrw4ixj652xmrsipql2yehala3hfmnha',
-            'ipfs://bafybeic7qi7vh6k45jn5zjbh3monyirlgxobpnirvd6xxnfpczcl7e6pu4',
-            'ipfs://bafybeihn3cnwpef5x7xhbzuomtravkwqzubfhldcujsm547hw7ighdxwpq',
-            'ipfs://bafybeih3lqc2awcm426fkcwyckgcolrsladamhvu2m5woi344nczndhcl4',
-            'ipfs://bafybeib2l2tynrgua5bm3bsxkj3tmtcbjrgrig4slu25g5mtgfcepyuooa',
-            'ipfs://bafkreibmdzd4eafjq53xpfvvhvjf3vfyjgfgcafttkemwvq5utnda7mqha',
-            'ipfs://bafybeia3pupyvyagfmmgcauw5veibugkec3vz6as5q3vs44233tyth26dm'
+            "ipfs://bafybeicefp4gw74adzrneu4mgm4crhdorablnpndrp5jz735gy3gfypkba",
+            "ipfs://bafybeigu7j3q3bur7xbxd4jrebkgin63zdj2jvnyu3xvp5enozzgznprqm",
+            "ipfs://bafybeiddqbyqcwzcw3o7otbmdncfkix2wtbgsts7ol3uggeztblq2na6ga",
+            "ipfs://bafybeigr65ngxeeuak3dyfu6rghvzly7pyxeqwqdl4u4mxjb5xjua4ksia",
+            "ipfs://bafybeicrtxwekyquafup53vi5pgrw4ixj652xmrsipql2yehala3hfmnha",
+            "ipfs://bafybeic7qi7vh6k45jn5zjbh3monyirlgxobpnirvd6xxnfpczcl7e6pu4",
+            "ipfs://bafybeihn3cnwpef5x7xhbzuomtravkwqzubfhldcujsm547hw7ighdxwpq",
+            "ipfs://bafybeih3lqc2awcm426fkcwyckgcolrsladamhvu2m5woi344nczndhcl4",
+            "ipfs://bafybeib2l2tynrgua5bm3bsxkj3tmtcbjrgrig4slu25g5mtgfcepyuooa",
+            "ipfs://bafkreibmdzd4eafjq53xpfvvhvjf3vfyjgfgcafttkemwvq5utnda7mqha",
+            "ipfs://bafybeia3pupyvyagfmmgcauw5veibugkec3vz6as5q3vs44233tyth26dm"
         ];
         _totalImagesAvailable = _imageURIs.length; // Initialisation avec le nombre total d'images
     }
 
-    function safeMint() public onlyOwner {
+    function publicMint() public {
+        // require(msg.value >= mintPrice, "Insufficient funds to mint.");
+        // require(totalSupply() < maxSupply, "Max supply reached.");
+
+        // Additional checks or logic can be added here
+
+        // Call the internal minting function
+        _safeMint(msg.sender);
+    }
+
+    function safeMint(address to) public onlyOwner {
         require(_totalImagesAvailable > 0, "All images have been assigned"); // Vérification du nombre d'images disponibles
 
         uint256 tokenId = _nextTokenId++;
-        _safeMint(msg.sender, tokenId);
+        _safeMint(to, tokenId);
 
         uint256 imageIndex = _randomIndex(tokenId);
         _setTokenURI(tokenId, _imageURIs[imageIndex]);
@@ -47,14 +56,20 @@ contract GROUPE3 is ERC721, ERC721URIStorage, Ownable {
     }
 
     function _randomIndex(uint256 tokenId) internal view returns (uint256) {
-        uint256 random = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, tokenId))) % _imageURIs.length;
+        uint256 random = uint256(
+            keccak256(abi.encodePacked(block.timestamp, msg.sender, tokenId))
+        ) % _imageURIs.length;
         while (_imageAssigned[random]) {
             random = (random + 1) % _imageURIs.length;
         }
         return random;
     }
 
-    function getAllCollectionData() public view returns (uint256[] memory, string[] memory) {
+    function getAllCollectionData()
+        public
+        view
+        returns (uint256[] memory, string[] memory)
+    {
         uint256[] memory ids = new uint256[](_mintedTokenIds.length);
         string[] memory uris = new string[](_mintedTokenIds.length);
 
@@ -66,21 +81,15 @@ contract GROUPE3 is ERC721, ERC721URIStorage, Ownable {
         return (ids, uris);
     }
 
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
         return super.tokenURI(tokenId);
     }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (bool)
-    {
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, ERC721URIStorage) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 }
