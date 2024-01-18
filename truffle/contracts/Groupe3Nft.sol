37fb571ed8423b3b30e224f324f10c13359051a5
Thomas Aubert <thomas.aubert@edu.devinci.fr>
2024-01-18 15:30:43 +0100

    :wrench: Added base uri function

--- truffle/contracts/Groupe3Nft.sol
+++ truffle/contracts/Groupe3Nft.sol
@@ -5,38 +5,53 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-
 contract GROUPE3 is ERC721, ERC721URIStorage, Ownable {
     uint256 private _nextTokenId;
+    // Ajout d'un tableau pour stocker les IDs de tokens mintés
+    uint256[] private _mintedTokenIds;
 
-    constructor(address initialOwner)
-        ERC721("GROUPE3", "GP3")
-        Ownable(initialOwner)
-    {}
+    constructor(
+        address initialOwner
+    ) ERC721("GROUPE3", "GP3") Ownable(initialOwner) {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "";
+    }
 
     function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
+        // Enregistrer l'ID du token minté
+        _mintedTokenIds.push(tokenId);
     }
 
-    // The following functions are overrides required by Solidity.
-
-    function tokenURI(uint256 tokenId)
+    // Fonction pour obtenir toutes les données de la collection
+    function getAllCollectionData()
         public
         view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
+        returns (uint256[] memory, string[] memory)
     {
+        uint256[] memory ids = new uint256[](_mintedTokenIds.length);
+        string[] memory uris = new string[](_mintedTokenIds.length);
+
+        for (uint256 i = 0; i < _mintedTokenIds.length; i++) {
+            ids[i] = _mintedTokenIds[i];
+            uris[i] = tokenURI(_mintedTokenIds[i]);
+        }
+
+        return (ids, uris);
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
         return super.tokenURI(tokenId);
     }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (bool)
-    {
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, ERC721URIStorage) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 }
